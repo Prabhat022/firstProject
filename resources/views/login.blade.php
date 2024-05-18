37707d059f5373c5af15a4cfd90d129b45115adf
prabhat022 <prabhatk.tiwari022@gmail.com>
2024-05-18 19:11:12 +0530

    update login file

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -18,10 +18,10 @@
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav">
                 <li class="nav-item active">
-                    <a class="nav-link" href="{{url('/register')}}" style="color:white">Register<span class="sr-only">(current)</span></a>
+                    <a class="nav-link" href="{{url('/register')}}" style="color:white">Register User<span class="sr-only">(current)</span></a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="{{url('/student/create')}}" style="color:white">Student Registration</a>
+                    <a class="nav-link" href="{{url('/student/create')}}" style="color:white">New Student Registration</a>
                 </li>
                 </ul>
             </div>
